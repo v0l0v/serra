cba0ceb4531748d2060d465dbff602de2104ed33
v0l0v <vict0rl0pez@protonmail.com>
2024-05-26 14:28:20 +0200

    mejorando reproduccion en p13 autoplay-10C

--- audioControl.js
+++ audioControl.js
@@ -1,11 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() { var audio = 
-    document.getElementById("audio"); var button = document.getElementById("audioControl"); var 
-    icon = button.querySelector("i");
+document.addEventListener("DOMContentLoaded", function() {
+    var audio = document.getElementById("audio");
+    var button = document.getElementById("audioControl");
+    var icon = button.querySelector("i");
+
     // Reproducir audio al cargar la página
-    audio.play(); button.addEventListener("click", function() { if (audio.paused) { 
-            audio.play(); icon.classList.remove("fa-play"); icon.classList.add("fa-pause");
+    audio.play();
+
+    button.addEventListener("click", function() {
+        if (audio.paused) {
+            audio.play();
+            icon.classList.remove("fa-play");
+            icon.classList.add("fa-pause");
         } else {
-            audio.pause(); icon.classList.remove("fa-pause"); icon.classList.add("fa-play");
+            audio.pause();
+            icon.classList.remove("fa-pause");
+            icon.classList.add("fa-play");
         }
     });
 });
